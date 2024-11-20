af43e357ab259d220f9c3478a46be823d507bf1c
LucasPereira01 <lucaspdocarmo@gmail.com>
2024-11-20 10:08:41 -0300

    Projeto Front End Fatec

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('nav ul li a').forEach((link) => {
+  link.addEventListener('click', function () {
+    document
+      .querySelectorAll('nav ul li a')
+      .forEach((item) => item.classList.remove('active'));
+    this.classList.add('active');
+  });
+});
